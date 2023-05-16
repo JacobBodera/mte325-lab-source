7717b5836ecc1311220f50f0b1acfc29710899dd
JacobBodera <boderajacob@gmail.com>
2023-05-16 14:06:56 -0400

    Initial commit for lab 1

---
+++ Lab 1/STM32CubeExpansion_SPN2_V1.1.1/Projects/STM32F401RE-Nucleo/Examples/MotionControl/MicrosteppingMotor/SW4STM32/X-CUBE-SPN2_F401/Debug/Example/SW4STM32/startup_stm32f401xe.d
@@ -0,0 +1,2 @@
+Example/SW4STM32/startup_stm32f401xe.o: \
+ /Users/jacobbodera/Desktop/MTE325/Lab\ 1/STM32CubeExpansion_SPN2_V1.1.1/Projects/STM32F401RE-Nucleo/Examples/MotionControl/MicrosteppingMotor/SW4STM32/startup_stm32f401xe.s
---
+++ Lab 1/STM32CubeExpansion_SPN2_V1.1.1/Projects/STM32F401RE-Nucleo/Examples/MotionControl/MicrosteppingMotor/SW4STM32/X-CUBE-SPN2_F401/Debug/Example/SW4STM32/syscalls.d
@@ -0,0 +1,2 @@
+Example/SW4STM32/syscalls.o: \
+ /Users/jacobbodera/Desktop/MTE325/Lab\ 1/STM32CubeExpansion_SPN2_V1.1.1/Projects/STM32F401RE-Nucleo/Examples/MotionControl/MicrosteppingMotor/SW4STM32/syscalls.c
